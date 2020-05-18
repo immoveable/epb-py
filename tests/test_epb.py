2db3314d1b669eb94b80e2372c21495d002d2cc1
Arthur White <arthur@white.li>
2020-05-18 17:41:59 +0200

    Init

---
+++ tests/test_epb.py
@@ -0,0 +1,9 @@
+from epb.main import Regulator, energy_class, total_consumption
+
+
+def test_energy_class():
+    assert energy_class(Regulator.BRUSSELS, 100) == "C+"
+
+
+def test_total_consumption():
+    assert total_consumption(100, 250) == 25_000
